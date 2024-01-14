2fe36c4e13c5630369e3c57b77c7645bdcf359a7
akkyxxx0418 <123301315+akkyxxx0418@users.noreply.github.com>
2024-01-15 03:31:32 +0900

    各画面レイアウト調整

--- next-app/src/app/06_restaurantList/page.jsx
+++ next-app/src/app/06_restaurantList/page.jsx
@@ -14,7 +14,7 @@ export default function Page(){
     return(
         <Mockupphone> {/*デモ用スマホ画面*/}
 
-        <div style={{  position: 'absolute', bottom: '0', width: '100%', zIndex: '100', marginLeft: '6px' }}>
+        <div style={{  position: 'absolute', bottom: '0', width: '100%', zIndex: '100'}}>
             <BottomAppBar />{/*下部メニューバー*/}
         </div>
 
